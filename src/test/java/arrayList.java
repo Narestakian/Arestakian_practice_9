449f3965f1be1265599ec4f5022642d81da0b49d
narestakian <n.arestakian@gmail.com>
2024-11-06 09:45:49 +0400

    task_1_Nino

---
+++ src/test/java/arrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        System.out.println(colors);
+        System.out.println( "Second  color:" + " " + colors.get(1));
+        colors.remove(0);
+        System.out.println("After removal:" + " " + colors);
+    }
+}
